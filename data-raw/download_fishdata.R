67115fe57483b9ca2c0bc6765ee7728cd9376d7c
LOMMELEN <els.lommelen@inbo.be>
2019-04-24 14:36:46 +0200

    data uit databank opslaan in package

---
+++ data-raw/download_fishdata.R
@@ -0,0 +1,48 @@
+# This script downloads measurement data on fishes that serve as test data
+
+library(RODBC)
+library(tidyverse)
+connection_VIS <-
+  odbcDriverConnect(
+    "Driver=SQL Server;Server=INBO-SQL08-PRD.inbo.be;Database=W0001_00_Vis;Trusted_Connection=Yes;"
+  )
+
+query_sample <-
+  "SELECT dw.Begindatum, dw.Einddatum, dw.WaarnemingKey, dw.BreedteTransect,
+    dw.LengteTraject, dw.BreedteRivier, dG.Helling, dW.AantalFuiken,
+    dW.AantalDagen, dM.Methodenaam, dG.Stilstaand, dG.Getijdewater, dG.Bekken,
+    dG.Brak
+  FROM DimWaarneming dW
+    LEFT JOIN DimGebied dG
+      ON dW.GebiedKey = dG.GebiedKey
+    LEFT JOIN DimMethode dM
+      ON dW.MethodeKey = dM.MethodeKey
+  WHERE dw.IsCurrent = 1 AND dG.IsCurrent = 1;"
+
+data_sample <-
+  sqlQuery(connection_VIS, query_sample, stringsAsFactors = FALSE)
+
+query_fish <-
+  "SELECT FM.WaarnemingKey, FM.MetingID, dV.Variabelecode, FM.Waarde, dT.Taxoncode
+  FROM FactMeting FM
+    INNER JOIN DimVariabele dV
+      ON FM.VariabeleKey = dV.VariabeleKey
+    LEFT JOIN DimTaxon dT
+      ON FM.TaxonKey = dT.TaxonKey
+  WHERE FM.IsCurrent = 1 AND dV.IsCurrent = 1
+    AND dV.Variabelegroep LIKE 'Hugo%';"
+
+data_fish <-
+  sqlQuery(connection_VIS, query_fish, stringsAsFactors = FALSE)
+data_fish <- data_fish %>%
+  spread(key = Variabelecode, value = Waarde) %>%
+  transmute(
+    WaarnemingKey, MetingID, Taxoncode,
+    Number = TAXONAANTAL,
+    Length = TAXONLEN,
+    Weight = ifelse(!is.na(TAXONGEW), TAXONGEW, TAXONTOTGEW)
+  )
+
+odbcClose(connection_VIS)
+
+usethis::use_data(data_sample, data_fish, internal = TRUE, overwrite = TRUE)
