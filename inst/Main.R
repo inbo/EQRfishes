01f0bb469027512bd5ce9d5e9b167a502a9f99b7
LOMMELEN <els.lommelen@inbo.be>
2019-09-17 15:59:36 +0200

    testcode in main op selectie van gegevens

--- inst/Main.R
+++ inst/Main.R
@@ -2,4 +2,16 @@
 library(EQRfishes)
 load(file = "inst/extrafiles/visdata.Rdata")
 
-results_eqr <- calculate_eqr(data_sample, data_fish)
+results_eqr <-
+  calculate_eqr(data_sample[c(29:30,32,34:36,39:41,43:62,477), ], data_fish)
+
+library(tidyverse)
+results_eqr_yzer <-
+  calculate_eqr(
+    data_sample_new %>%
+      filter(.data$IndexTypeCode == "YZRP"),
+    data_fish %>%
+      mutate(
+        sample_key = .data$sample_key_new,
+        sample_key_new = NULL
+      )
