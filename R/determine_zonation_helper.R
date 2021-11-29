bb8ba5214d39d31ad91f785d3025ed6454accf10
ElsLommelen <els.lommelen@inbo.be>
2021-11-29 14:43:42 +0100

    rename determine_zonation() to determine_zonation_helper() and add functionality to choose between old and new version

--- R/determine_zonation_helper.R
+++ R/determine_zonation_helper.R
@@ -1,11 +1,17 @@
-#' determine the zonation of the location
+#' determine the zonation of one location
 #'
-#' This function determines the zonation based on the given measurements for one location.
+#' This helper function determines the zonation based on the given measurements
+#' for one location.
+#' To determine the zonation of the whole dataset at once, please use function
+#' `determine_zonation`.
 #'
 #' @param var_width width of the river at the sample location
 #' @param var_slope slope of the river at the sample location
 #' @param var_tidal tidal effect present (TRUE) or absent (FALSE)?
 #' @param var_indextype type based on salinity of the sample location
+#' @param version 'new' version with bron and brabeel, or 'old' version without
+#' these two zonations?
+#' Defaults to 'new'.
 #'
 #' @return zonation of the focal location(s)
 #'
@@ -16,9 +22,11 @@
 #'
 #' @export
 #'
-determine_zonation <-
-  function(var_width, var_slope, var_tidal, var_indextype) {
+determine_zonation_helper <-
+  function(var_width, var_slope, var_tidal, var_indextype,
+           version = c("new", "old")) {
 
+  match.arg(version)
   data_zonation <-
     suppressMessages(
       read_csv2(system.file("extdata/data_zonation.csv", package = "EQRfishes"))
@@ -38,5 +46,9 @@ determine_zonation <-
     return("(undetermined)")
   }
 
-  return(data_zonation$zonation)
+  if (version[1] == "new") {
+    return(data_zonation$zonation)
+  } else {
+    return(data_zonation$zonation_old)
+  }
 }
