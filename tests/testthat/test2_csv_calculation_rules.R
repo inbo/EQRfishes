8ad41e0c984549d64ce0aca45adb4f57eea9df52
INBO <info@inbo.be>
2023-05-23 11:16:06 +0200

    specify join relationship (to avoid warning)

--- tests/testthat/test2_csv_calculation_rules.R
+++ tests/testthat/test2_csv_calculation_rules.R
@@ -205,7 +205,8 @@ describe("variables exist in dependent tables", {
               )
           ) %>%
           distinct(),
-        by = c("metric_score")
+        by = c("metric_score"),
+        relationship = "many-to-many"
       ) %>%
       filter(is.na(.data$metric_source)) %>%
       transmute(
