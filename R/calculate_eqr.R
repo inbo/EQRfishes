51ef51c104c4d949f0ada59cbe14a41ceebc1690
ElsLommelen <els.lommelen@inbo.be>
2023-06-29 10:26:34 +0200

    aggregate results for estuaries, lakes and canals

--- R/calculate_eqr.R
+++ R/calculate_eqr.R
@@ -196,6 +196,34 @@ calculate_eqr <-
     ) %>%
     ungroup()
 
+  #for the new method (estuaries, lakes and canals), results are aggregated
+  result_metrics_aggregated <- result_metrics %>%
+    filter(!is.na(.data$method_for_metric)) %>%
+    mutate(
+      sample_key = substr(.data$sample_key, 1, nchar(.data$sample_key) - 10)
+    ) %>%
+    group_by(
+      .data$sample_key, .data$zonation, .data$LocationID,
+      .data$metric_name, .data$metric_score_name, .data$method_for_metric
+    ) %>%
+    summarise(
+      metric_value =
+        ifelse(
+          all(is.na(.data$metric_value)),
+          as.character(NA),
+          max(.data$metric_value, na.rm = TRUE)
+        ),
+      metric_score =
+        ifelse(
+          all(is.na(.data$metric_score)),
+          as.character(NA),
+          max(.data$metric_score, na.rm = TRUE)
+        )
+    )
+
+  result_metrics %<>%
+    filter(is.na(.data$method_for_metric)) %>%
+    bind_rows(result_metrics_aggregated)
 
   eqr_scores <-
     suppressMessages(
