6cdfe9548977e1be5632e0deae734133594e017f
ElsLommelen <els.lommelen@inbo.be>
2025-04-28 14:27:32 +0200

    replace uppercase by lowercase in filename

--- inst/main.R
+++ inst/main.R
@@ -7,6 +7,7 @@ data_sample_zonation <- determine_zonation(data_sample[1:500, ], "old")
 results_eqr <-
   calculate_eqr(data_sample_zonation[c(29:30,32,34:36,39:41,43:62,477), ], data_fish)
 
+#onderstaande vervangen door nieuw voorbeeld, of alles in voorbeeld van documentatie van functie?
 library(tidyverse)
 results_eqr_yzer <-
   calculate_eqr(
