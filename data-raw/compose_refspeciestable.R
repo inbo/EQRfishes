36507be7f5a0639bee9fc4a81b2d191865ca773d
ElsLommelen <els.lommelen@inbo.be>
2025-05-13 17:34:05 +0200

    clean table data_taxonmetrics.csv (remove unused columns, use taxonnames from VIS,...)

--- data-raw/compose_refspeciestable.R
+++ data-raw/compose_refspeciestable.R
@@ -10,14 +10,27 @@ connection_vis <-
     "Driver=SQL Server;Server=INBO-SQL08-PRD.inbo.be;Database=W0001_00_Vis;Trusted_Connection=Yes;" # nolint: line_length_linter
   )
 
+query_species <-
+  "SELECT TaxonKey, Taxoncode, Soort, WetenschappelijkeNaam
+  FROM dbo.DimTaxon"
+
+list_species <-
+  sqlQuery(connection_vis, query_species, stringsAsFactors = FALSE) %>%
+  rename(
+    taxonkey = TaxonKey, taxoncode = Taxoncode,
+    taxonname = WetenschappelijkeNaam, dutch_name = Soort
+  )
+
 query_taxonmetrics <-
   "SELECT *
   FROM visindex.data_taxonmetrics;"
 
 data_taxonmetrics <-
   sqlQuery(connection_vis, query_taxonmetrics, stringsAsFactors = FALSE) %>%
-  select(-data_taxonmetricsKey, -taxonKey, -soort) %>%
-  rename(taxonname = wetenschappelijke_naam)
+  select(
+    -data_taxonmetricsKey, -wetenschappelijke_naam, -soort, taxonkey = taxonKey,
+    -opmerking
+  )
 
 data_taxonmetrics %>%
   count(taxoncode) %>%
@@ -29,8 +42,11 @@ query_brabeel <-
 
 data_brabeel <-
   sqlQuery(connection_vis, query_brabeel, stringsAsFactors = FALSE) %>%
-  select(-brabeel_metriekenKey, -TaxonKey, -Soortenlijst_brabeel) %>%
-  rename(taxonname = Wetenschappelijke_naam_brabeel, taxoncode = TaxonCode)
+  select(
+    -brabeel_metriekenKey, -Wetenschappelijke_naam_brabeel,
+    -Soortenlijst_brabeel
+  ) %>%
+  rename(taxoncode = TaxonCode, taxonkey = TaxonKey)
 
 data_brabeel %>%
   count(taxoncode) %>%
@@ -42,8 +58,11 @@ query_brongebied <-
 
 data_brongebied <-
   sqlQuery(connection_vis, query_brongebied, stringsAsFactors = FALSE) %>%
-  select(-brongebied_metriekenKey, -TaxonKey, -Soortenlijst_brongebied) %>%
-  rename(taxonname = Wetenschappelijke_naam_brongebied, taxoncode = TaxonCode)
+  select(
+    -brongebied_metriekenKey, -Wetenschappelijke_naam_brongebied,
+    -Soortenlijst_brongebied
+  ) %>%
+  rename(taxonkey = TaxonKey, taxoncode = TaxonCode)
 
 data_brongebied %>%
   count(taxoncode) %>%
@@ -57,10 +76,12 @@ data_getijde_zijrivieren_zoet <- sqlQuery(
   connection_vis, query_getijde_zijrivieren_zoet, stringsAsFactors = FALSE
 ) %>%
   select(
-    -getijde_zijrivieren_zoetKey, -TaxonKey, -Soortenlijst_Getijde_rivieren_zoet
+    -getijde_zijrivieren_zoetKey,
+    -Wetenschappelijke_naam_Getijde_zijrivieren_zoet,
+    -Soortenlijst_Getijde_rivieren_zoet
   ) %>%
   rename(
-    taxonname = Wetenschappelijke_naam_Getijde_zijrivieren_zoet,
+    taxonkey = TaxonKey,
     taxoncode = TaxonCode
   )
 
@@ -74,10 +95,9 @@ query_ijzer <-
 
 data_ijzer <-
   sqlQuery(connection_vis, query_ijzer, stringsAsFactors = FALSE) %>%
-  select(-ijzerestuarium_metriekenKey, -TaxonKey) %>%
-  mutate(
-    WetenschappelijkeNaam = trimws(WetenschappelijkeNaam),
-    opmerking = trimws(opmerking)
+  select(
+    -ijzerestuarium_metriekenKey, -WetenschappelijkeNaam, taxonkey = TaxonKey,
+    -NederLandseNaam, -opmerking
   )
 
 data_ijzer %>%
@@ -91,8 +111,8 @@ query_mesohalien <-
 data_mesohalien <-
   sqlQuery(connection_vis, query_mesohalien, stringsAsFactors = FALSE) %>%
   select(
-    -zeeschelde_mesohalien_metriekenKey, -TaxonKey, -NederlandseNaam,
-    -WetenschappelijkeNaam
+    -zeeschelde_mesohalien_metriekenKey, taxonkey = TaxonKey, -NederlandseNaam,
+    -WetenschappelijkeNaam, -opmerkingen
   )
 
 data_mesohalien %>%
@@ -106,7 +126,7 @@ query_oligohalien <-
 data_oligohalien <-
   sqlQuery(connection_vis, query_oligohalien, stringsAsFactors = FALSE) %>%
   select(
-    -zeeschelde_oligohalien_metriekenKey, -TaxonKey, -NederlandseNaam,
+    -zeeschelde_oligohalien_metriekenKey, taxonkey = TaxonKey, -NederlandseNaam,
     -WetenschappelijkeNaam
   )
 
@@ -120,9 +140,12 @@ query_zeeschelde_zoet <-
 
 data_zeeschelde_zoet <-
   sqlQuery(connection_vis, query_zeeschelde_zoet, stringsAsFactors = FALSE) %>%
-  select(-zeeschelde_zoet_metriekenKey, -TaxonKey, -Nederlandse_naam) %>%
+  select(
+    -zeeschelde_zoet_metriekenKey, -Wetenschappelijke_Naam_Zeeschelde_zoet,
+    -Nederlandse_naam, -opmerking
+  ) %>%
   rename(
-    taxonname = Wetenschappelijke_Naam_Zeeschelde_zoet, taxoncode = TaxonCode
+    taxonkey = TaxonKey, taxoncode = TaxonCode
   )
 
 data_zeeschelde_zoet %>%
@@ -133,30 +156,27 @@ odbcClose(connection_vis)
 
 data_taxonmetrics <- data_taxonmetrics %>%
   full_join(
-    data_brabeel %>%
-      select(-"taxonname"),
-    by = c("taxoncode")
+    data_brabeel,
+    by = c("taxonkey", "taxoncode")
   ) %>%
   full_join(
-    data_brongebied %>%
-      select(-"taxonname"),
-    by = c("taxoncode")
+    data_brongebied,
+    by = c("taxonkey", "taxoncode")
   ) %>%
   full_join(
-    data_getijde_zijrivieren_zoet %>%
-      select(-"taxonname"),
-    by = c("taxoncode")
+    data_getijde_zijrivieren_zoet,
+    by = c("taxonkey", "taxoncode")
   ) %>%
-  full_join(data_ijzer, by = c("taxoncode")) %>%
-  full_join(data_mesohalien, by = c("taxoncode")) %>%
-  full_join(data_oligohalien, by = c("taxoncode")) %>%
+  full_join(data_ijzer, by = c("taxonkey", "taxoncode")) %>%
+  full_join(data_mesohalien, by = c("taxonkey", "taxoncode")) %>%
+  full_join(data_oligohalien, by = c("taxonkey", "taxoncode")) %>%
   full_join(
-    data_zeeschelde_zoet %>%
-      select(-"taxonname"),
-    by = c("taxoncode")
+    data_zeeschelde_zoet,
+    by = c("taxonkey", "taxoncode")
   ) %>%
   mutate_if(is.numeric, coalesce, 0) %>%
   filter(.data$taxoncode != "HYB.HYB.") %>%
-  arrange(taxoncode)
+  arrange(taxoncode) %>%
+  left_join(list_species, by = c("taxonkey", "taxoncode"))
 
 write_csv2(data_taxonmetrics, "inst/extdata/data_taxonmetrics.csv")
